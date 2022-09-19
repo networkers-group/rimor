2f4456dcddeeb53d19c8eea2513d6a27f53e38e3
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-09-20 00:50:46 +0200

    Create CommandExecutor.java
    Co-authored by: lutzseverino <jasperlutzseverino@gmail.com>

---
+++ rimor/src/main/java/st/networkers/rimor/internal/CommandExecutor.java
@@ -0,0 +1,38 @@
+package st.networkers.rimor.internal;
+
+import st.networkers.rimor.context.ExecutionContext;
+import st.networkers.rimor.internal.command.instruction.CommandInstruction;
+import st.networkers.rimor.internal.provide.ParameterProviderRegistry;
+import st.networkers.rimor.internal.reflect.CachedMethod;
+import st.networkers.rimor.internal.reflect.CachedParameter;
+import st.networkers.rimor.util.InjectionUtils;
+import st.networkers.rimor.util.ReflectionUtils;
+
+public class CommandExecutor {
+
+    private final ParameterProviderRegistry parameterProviderRegistry;
+
+    public CommandExecutor(ParameterProviderRegistry parameterProviderRegistry) {
+        this.parameterProviderRegistry = parameterProviderRegistry;
+    }
+
+    public Object execute(CommandInstruction instruction, ExecutionContext context) {
+        CachedMethod cachedMethod = instruction.getMethod();
+
+        Object[] parameters = InjectionUtils.resolve(
+                cachedMethod,
+                context,
+                parameter -> this.fromProvider(parameter, context)
+        );
+
+        return ReflectionUtils.invoke(cachedMethod.getMethod(), instruction.getCommandInstance(), parameters);
+    }
+
+    private Object fromProvider(CachedParameter parameter, ExecutionContext context) {
+        return parameterProviderRegistry.get(parameter.getType()).stream()
+                .filter(provider -> provider.canProvide(parameter))
+                .findAny()
+                .map(provider -> provider.get(parameter, context))
+                .orElse(null);
+    }
+}
