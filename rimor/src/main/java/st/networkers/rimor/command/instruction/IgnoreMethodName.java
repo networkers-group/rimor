e86a46632dae15e773f25d0a98a5b0911f663c84
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-09-20 00:38:06 +0200

    Create main command annotations
    Co-authored by: lutzseverino <jasperlutzseverino@gmail.com>

---
+++ rimor/src/main/java/st/networkers/rimor/command/instruction/IgnoreMethodName.java
@@ -0,0 +1,14 @@
+package st.networkers.rimor.command.instruction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Instruction methods with this annotation won't use the method's name as an alias.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface IgnoreMethodName {
+}
