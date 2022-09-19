a4c69441c7788c9136fcc508a1dab5b9ddf96a20
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-09-20 00:50:51 +0200

    Create ParamProvider.java
    Co-authored-by: lutzseverino <jasperlutzseverino@gmail.com>

---
+++ rimor/src/main/java/st/networkers/rimor/internal/command/parameter/ParamProvider.java
@@ -0,0 +1,19 @@
+package st.networkers.rimor.internal.command.parameter;
+
+import st.networkers.rimor.provide.RequireAnnotations;
+import st.networkers.rimor.command.parameter.Param;
+import st.networkers.rimor.command.parameter.Params;
+import st.networkers.rimor.provide.ProvidesParameter;
+
+import java.util.List;
+
+public class ParamProvider {
+
+    @ProvidesParameter
+    @RequireAnnotations(Param.class)
+    public String provide(Param param, @Params List<String> commandParameters) {
+        int position = param.value();
+
+        return position < commandParameters.size() ? commandParameters.get(position) : null;
+    }
+}
