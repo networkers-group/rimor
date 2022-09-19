14d22292d09ec68305edd4ff00a9ceddd45f8388
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-09-20 00:50:54 +0200

    Create Rimor.java
    Co-authored-by: lutzseverino <jasperlutzseverino@gmail.com>

---
+++ rimor/src/main/java/st/networkers/rimor/Rimor.java
@@ -0,0 +1,31 @@
+package st.networkers.rimor;
+
+import st.networkers.rimor.command.Command;
+import st.networkers.rimor.context.ExecutionContext;
+import st.networkers.rimor.internal.CommandExecutor;
+import st.networkers.rimor.internal.CommandRegistry;
+import st.networkers.rimor.internal.CommandResolver;
+import st.networkers.rimor.internal.command.instruction.CommandInstruction;
+import st.networkers.rimor.internal.command.parameter.ParamProvider;
+import st.networkers.rimor.internal.provide.ParameterProviderRegistry;
+
+public class Rimor {
+
+    private final CommandRegistry registry = new CommandRegistry();
+    private final CommandExecutor executor;
+
+    public Rimor() {
+        ParameterProviderRegistry parameterProviderRegistry = new ParameterProviderRegistry();
+        parameterProviderRegistry.register(new ParamProvider());
+
+        this.executor = new CommandExecutor(parameterProviderRegistry);
+    }
+
+    public void registerCommand(Command command) {
+        registry.registerCommand(CommandResolver.resolve(command));
+    }
+
+    public Object execute(CommandInstruction instruction, ExecutionContext context) {
+        return this.executor.execute(instruction, context);
+    }
+}
