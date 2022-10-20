a388fdb7736accddddbadef08d5ac10baef15344
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-10-20 13:44:57 +0200

    feat: add PluginManager#getPlugin

--- rimor/src/main/java/st/networkers/rimor/plugin/PluginManager.java
+++ rimor/src/main/java/st/networkers/rimor/plugin/PluginManager.java
@@ -6,6 +6,8 @@ public interface PluginManager {
 
     void registerPlugin(RimorPlugin plugin);
 
+    <T extends RimorPlugin> T getPlugin(Class<T> pluginClass);
+
     void unregisterPlugin(RimorPlugin plugin);
 
     Collection<RimorPlugin> getRegisteredPlugins();
--- rimor/src/main/java/st/networkers/rimor/plugin/PluginManagerImpl.java
+++ rimor/src/main/java/st/networkers/rimor/plugin/PluginManagerImpl.java
@@ -2,13 +2,14 @@ package st.networkers.rimor.plugin;
 
 import st.networkers.rimor.Rimor;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PluginManagerImpl implements PluginManager {
 
     private final Rimor rimor;
-    private final Collection<RimorPlugin> plugins = new ArrayList<>();
+    private final Map<Class<? extends RimorPlugin>, RimorPlugin> plugins = new HashMap<>();
 
     public PluginManagerImpl(Rimor rimor) {
         this.rimor = rimor;
@@ -19,16 +20,22 @@ public class PluginManagerImpl implements PluginManager {
         plugin.configure(rimor);
         plugin.getCommands().forEach(rimor::registerCommand);
         plugin.getProviders().forEach(rimor::registerProvider);
-        plugins.add(plugin);
+        this.plugins.put(plugin.getClass(), plugin);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends RimorPlugin> T getPlugin(Class<T> pluginClass) {
+        return (T) this.plugins.get(pluginClass);
     }
 
     @Override
     public void unregisterPlugin(RimorPlugin plugin) {
-        plugins.remove(plugin);
+        this.plugins.remove(plugin.getClass());
     }
 
     @Override
     public Collection<RimorPlugin> getRegisteredPlugins() {
-        return this.plugins;
+        return this.plugins.values();
     }
 }
