6259c7a9d1749bb48b21985dc7a20b66f3bfd508
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-10-26 16:15:54 +0200

    feat: add InstructionNotFoundException

---
+++ rimor/src/main/java/st/networkers/rimor/interpret/InstructionNotFoundException.java
@@ -0,0 +1,45 @@
+package st.networkers.rimor.interpret;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import st.networkers.rimor.command.Command;
+
+/**
+ * Thrown if the execution path does not belong to any instruction and there are no main instructions for the command.
+ * <p>
+ * For example, the command
+ * <pre>
+ *     &#64;Aliases("git")
+ *     public class GitCommand extends AbstractCommand {
+ *         &#64;Instruction
+ *         public void commit(@Params List\<String> params) {
+ *              // whatever
+ *         }
+ *
+ *         &#64;Instruction
+ *         public void push(@Params List\<String> params) {
+ *              // whatever
+ *         }
+ *     }
+ * </pre>
+ * will throw an {@link InstructionNotFoundException} if {@code git pull} is executed.
+ * <p>
+ * However, if a main instruction is present:
+ * <pre>
+ *     &#64;Aliases("git")
+ *     public class GitCommand extends AbstractCommand {
+ *         &#64;MainInstruction
+ *         public void main(@Params List<String> params) {
+ *              // whatever
+ *         }
+ *     }
+ * </pre>
+ * will not throw anything if {@code git pull} is executed, because the main instruction will be run and (if using the
+ * rimor-params plugin), {@code "pull"} will be passed as a parameter.
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class InstructionNotFoundException extends RuntimeException {
+    private final Command uberCommand;
+    private final Command command;
+}
