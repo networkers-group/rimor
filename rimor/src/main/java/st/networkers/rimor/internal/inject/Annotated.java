e6ad40291359a9db9decb88ba86fa3768fec0d74
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-11-16 20:45:01 +0100

    docs: document Annotated

--- rimor/src/main/java/st/networkers/rimor/internal/inject/Annotated.java
+++ rimor/src/main/java/st/networkers/rimor/internal/inject/Annotated.java
@@ -3,6 +3,14 @@ package st.networkers.rimor.internal.inject;
 import java.lang.annotation.Annotation;
 import java.util.Collection;
 
+/**
+ * Represents a dynamically annotated type.
+ *
+ * @see st.networkers.rimor.inject.Token
+ * @see st.networkers.rimor.provide.RimorProvider
+ * @see st.networkers.rimor.interpret.execution.ExecutionEnclosingTask
+ * @see st.networkers.rimor.internal.instruction.Instruction
+ */
 public interface Annotated {
 
     /**
@@ -15,14 +23,39 @@ public interface Annotated {
      */
     boolean matchesAnnotations(Annotated annotated);
 
+    /**
+     * The annotation instances of this {@link Annotated}.
+     */
     Collection<Annotation> getAnnotations();
 
+    /**
+     * The classes of the required annotations of this {@link Annotated}.
+     */
     Collection<Class<? extends Annotation>> getRequiredAnnotations();
 
+    /**
+     * Gets the annotation instance present in this {@link Annotated} for the given {@link Annotation} type.
+     *
+     * @param annotationClass class of the annotation to get
+     * @return the annotation present in this {@link Annotated}, or null if not present.
+     */
     <A extends Annotation> A getAnnotation(Class<A> annotationClass);
 
+    /**
+     * Whether this {@link Annotated} contains the given {@link Annotation}, or contains the given {@link Annotation}
+     * class as a required annotation.
+     *
+     * @param annotation the annotation to check
+     * @return if this {@link Annotated} contains the given {@link Annotation}
+     */
     boolean isAnnotationPresent(Annotation annotation);
 
+    /**
+     * Whether this {@link Annotated} contains an instance of the given {@link Annotation} class, or contains the given
+     * {@link Annotation} class as a required annotation
+     *
+     * @param annotationClass the annotation type to check
+     * @return if this {@link Annotated} contains the given {@link Annotation}
+     */
     boolean isAnnotationPresent(Class<? extends Annotation> annotationClass);
-
 }
